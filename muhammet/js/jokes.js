0170204b8d8c93c0a7876836f72767fcdcee14e7
Muhammet Cin <muhammetacin@gmail.com>
2022-02-08 17:10:19 +0100

    Get random excuse on click btn

--- muhammet/js/jokes.js
+++ muhammet/js/jokes.js
@@ -1,24 +1,21 @@
+const excuseButton = document.getElementById("getExcuseBtn");
 const excuseTitle = document.getElementsByClassName("modal-title")[0];
 const excuseBody = document.getElementsByClassName("modal-body")[0];
 
 const excuseCategories = ["office", "family", "children", "college", "party"];
 let excuseCategory = "party";
 
-const fetchExcuse = fetch(
-  "https://excuser.herokuapp.com/v1/excuse/" + excuseCategory
-)
-  .then((response) => response.json())
-  .then((data) => {
-    setCategoryAsTitle(data[0].category);
-    setJoke(data[0].excuse);
-    console.log(data[0].excuse);
-  });
+const fetchExcuse = () => {
+  excuseCategory = excuseCategories[Math.floor(Math.random() * 5)];
 
-const fetchChuckNorris = fetch("https://api.chucknorris.io/jokes/random")
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data.value);
-  });
+  fetch("https://excuser.herokuapp.com/v1/excuse/" + excuseCategory)
+    .then((response) => response.json())
+    .then((data) => {
+      setCategoryAsTitle(data[0].category);
+      setJoke(data[0].excuse);
+      console.log(data[0].excuse);
+    });
+};
 
 const setCategoryAsTitle = (excuse) => {
   excuse = excuse.charAt(0).toUpperCase() + excuse.slice(1);
@@ -28,3 +25,11 @@ const setCategoryAsTitle = (excuse) => {
 const setJoke = (excuse) => {
   excuseBody.textContent = excuse;
 };
+
+excuseButton.addEventListener("click", fetchExcuse);
+
+const fetchChuckNorris = fetch("https://api.chucknorris.io/jokes/random")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data.value);
+  });
