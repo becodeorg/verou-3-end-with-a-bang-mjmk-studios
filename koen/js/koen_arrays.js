012154222aafd9fca9a8631dd1379bd361a8adb8
Wahala <khaelterman1981@gmail.com>
2022-02-08 10:52:39 +0100

    styling hardcode elements

---
+++ koen/js/koen_arrays.js
@@ -0,0 +1,8 @@
+let rooms = [
+    {
+        "room": "front of the classroom",
+        "description": "North of you are a row of desks. <span style='color: red;'>Basile</span> and <span style='color: red;'>Bert</span> are sitting here, typing away at their laptops. To the east is a <span style='color: red;'>whiteboard</span> attached to the wall. West of you are floor to ceiling windows with a seven story drop behind it.",
+        "directions": ["south", "west"]
+
+    }
+]
