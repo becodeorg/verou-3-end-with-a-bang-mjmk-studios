4693edca28a8fdf9ec71b4711a56ce757e180f27
Wahala <khaelterman1981@gmail.com>
2022-02-08 11:26:26 +0100

    JS - Create file with rooms-array

--- koen/js/koen_arrays.js
+++ koen/js/koen_arrays.js
@@ -2,7 +2,19 @@ let rooms = [
     {
         "room": "front of the classroom",
         "description": "North of you are a row of desks. <span style='color: red;'>Basile</span> and <span style='color: red;'>Bert</span> are sitting here, typing away at their laptops. To the east is a <span style='color: red;'>whiteboard</span> attached to the wall. West of you are floor to ceiling windows with a seven story drop behind it.",
-        "directions": ["south", "west"]
-
-    }
+        "directions": ["south", "west"],
+        "creatures": ["Bert is sitting here.", "Basile is sitting here."],
+    },
+    {
+        "room": "back of the classroom",
+        "description": "This part of the classroom is filled with standard <span style='color: red;'>tables</span> and chairs. There is really nothing special about them, just really avarage <span style='color: red;'>tables</span> and chairs. No need to <span style='color: rgb(153, 153, 0);'>examine</span> anything here. Especially not the <span style='color: red;'>tables</span>. West of you are floor to ceiling windows with a seven story drop behind it. East is a doorway leading to the elevators.",
+        "directions": ["north", "east", "west"],
+        "creatures": [],
+    },
+    {
+        "room": "elevator room",
+        "description": "This is a fairly empty room. To the south is the elevator <span style='color: red;'>door</span>. To the east through a doorway you can see <span style='color: red;'>Erwin</span>. He is on the phone with someone, yeah, so you probably shouldn't <span style='color: rgb(153, 153, 0);'>bother</span> him, ok, yeah.",
+        "directions": ["west"],
+        "creatures": ["Erwin is standing here."],
+    },
 ]
