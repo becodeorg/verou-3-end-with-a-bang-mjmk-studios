a378bb79e36def25adc88266c75fd04bc27b74d6
Wahala <khaelterman1981@gmail.com>
2022-02-08 14:56:37 +0100

    JS - Add function for input start

--- koen/js/koen_arrays.js
+++ koen/js/koen_arrays.js
@@ -1,4 +1,4 @@
-let rooms = [
+export const rooms = [
     {
         "room": "front of the classroom",
         "description": "North of you are a row of desks. <span style='color: red;'>Basile</span> and <span style='color: red;'>Bert</span> are sitting here, typing away at their laptops. To the east is a <span style='color: red;'>whiteboard</span> attached to the wall. West of you are floor to ceiling windows with a seven story drop behind it.",
