3e55b1b73062c680f62a6c3337a4e1d98f1fa628
Muhammet Cin <muhammetacin@gmail.com>
2022-02-09 10:02:59 +0100

    Add useless fact API

--- muhammet/README.md
+++ muhammet/README.md
@@ -1,3 +1,9 @@
 # Synthesizer
 
 A Synthesizer for you to play your favourite 16-bit game music!
+
+## API's
+
+- <https://excuser.herokuapp.com/>
+- <https://api.chucknorris.io/>
+- <https://uselessfacts.jsph.pl/>
--- muhammet/js/jokes.js
+++ muhammet/js/jokes.js
@@ -1,8 +1,11 @@
 import fetchExcuse from "./_random-excuse.js";
 import fetchChuckNorris from "./_chuck-jokes.js";
+import fetchUselessFact from "./_useless-fact.js";
 
 const excuseButton = document.getElementById("getExcuseBtn");
 const chuckButton = document.getElementById("chuckBtn");
+const uselessButton = document.getElementById("uselessBtn");
 
 excuseButton.addEventListener("click", fetchExcuse);
 chuckButton.addEventListener("click", fetchChuckNorris);
+uselessButton.addEventListener("click", fetchUselessFact);
