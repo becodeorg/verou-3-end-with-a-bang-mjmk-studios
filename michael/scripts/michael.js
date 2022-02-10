c17b7394fb5ed2740e7140723011f07ede025026
Angelgroenen <bm.bracke.michael@gmail.com>
2022-02-10 12:06:31 +0100

    API works

--- michael/scripts/michael.js
+++ michael/scripts/michael.js
@@ -2,13 +2,31 @@ const getBody = document.querySelector("body");
 console.log(getBody);
 
 const createDiv = document.createElement("div");
-createDiv.classList.add("API-container");
+createDiv.classList.add("API-container-one");
 getBody.appendChild(createDiv);
-createDiv.style.width = "500px";
-createDiv.style.height = "500px";
-createDiv.style.backgroundColor = "red";
-console.log(createDiv);
 
+console.log(createDiv);
+let randomWall = "https://wallpapercave.com/wp/wp1809893.jpg";
+/* createDiv.style.backgroundImage = "url(" + randomWall + ")"; */
+createDiv.style.height ="400px";
+createDiv.style.width = "200px";
+
+const createFirstImg = document.createElement("img");
+createFirstImg.classList.add("img-one");
+createDiv.appendChild(createFirstImg);
+createFirstImg.style.maxHeight = "100%";
+createFirstImg.style.maxWidth = "100%";
+createFirstImg.style.height ="400px";
+createFirstImg.style.width = "200px";
+/* createFirstImg.src = randomWall; */
+
+
+const createDivUpperLeft = document.createElement("div");
+createDivUpperLeft.classList.add("API-container-two");
+getBody.appendChild(createDivUpperLeft);
+createDivUpperLeft.style.backgroundImage = "url(" + randomWall + ")";
+createDivUpperLeft.style.height = "100px"
+createDivUpperLeft.style.width = "200px"
 
 
 
@@ -21,29 +39,54 @@ const submitButton = document.getElementById("search-button");
 console.log(submitButton);
 
 
-
+let finishedAnimeInput;
 const getAnimeWallpaper = (event) => {
   event.preventDefault();
   let getAnime = getSearch.value.toLowerCase();
-  console.log(getAnime);
-  getAnimeOutThere(getAnime);
-}
-
-const getAnimeOutThere = (getAnime) => {
-  console.log(getAnime);
-  return getAnime;
-}
-getAnimeOutThere();
-
-
-/* const changeAnimeString = (getAnime) => {
   getAnime = getAnime.replace(/\s+/g, "-");
-  
   finishedAnimeInput = getAnime + "-wallpapers";
-  console.log(finishedAnimeInput);
-}; 
+  callAnimeOutThere(finishedAnimeInput);
+  
+}
 
-changeAnimeString(); */
+
+submitButton.addEventListener('click', getAnimeWallpaper);
+    getSearch.addEventListener('keypress', function (e) {
+      if (e.code === 'Enter') {
+        getAnimeWallpaper(e);
+      }
+    });
+
+const callAnimeOutThere = (finishedAnimeInput) => {
+
+
+
+  fetch("https://yume-anime-wallpapers.p.rapidapi.com/manga_anime?slug=" + finishedAnimeInput, {
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-host": "yume-anime-wallpapers.p.rapidapi.com",
+        "x-rapidapi-key": "e1fc11060emsh56e7368925313f9p1174d1jsn3e6d83911114"
+      }
+    })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+     console.log (data[0].wallpapers);
+       const randomWallpaper = data[0].wallpapers[Math.floor(Math.random()*data[0].wallpapers.length)];
+      console.log (randomWallpaper); 
+     createFirstImg.src = randomWallpaper;
+      
+     
+      // hier nog iets doen met die data
+
+    })
+    .catch(err => {
+      console.error(err);
+    });
+};
+
+
+callAnimeOutThere();
 
 
 /* fetch("https://yume-anime-wallpapers.p.rapidapi.com/manga_anime", {
@@ -70,30 +113,9 @@ changeAnimeString(); */
       };
     });
     
+*/
 
 
-     const callAnimeOutThere = (finishedAnimeInput) => {
-
-
-      fetch("https://yume-anime-wallpapers.p.rapidapi.com/manga_anime?slug=" + finishedAnimeInput, {
-          "method": "GET",
-          "headers": {
-            "x-rapidapi-host": "yume-anime-wallpapers.p.rapidapi.com",
-            "x-rapidapi-key": "e1fc11060emsh56e7368925313f9p1174d1jsn3e6d83911114"
-          }
-        })
-        .then((response) => response.json())
-        .then((data) => {
-          console.log(data);
-          return data;
-          // hier nog iets doen met die data
-
-        })
-        .catch(err => {
-          console.error(err);
-        });
-    };
- */
 
     //TODO: add the additional values to the searched string
 
@@ -106,10 +128,5 @@ changeAnimeString(); */
       }
     } */
 
-    submitButton.addEventListener('click', getAnimeWallpaper);
-    getSearch.addEventListener('keypress', function (e) {
-      if (e.code === 'Enter') {
-        getAnimeWallpaper(e);
-      }
-    });
+    
     // TODO: write a function that takes the data from the searchbar and puts it in the API
